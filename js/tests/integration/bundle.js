516afb81194a2f964be26e556b53cd1b78d18e8d
gmcpo <gmcpo@LAPTOP-2AD376QK>
2019-03-14 18:17:32 +0000

    Initial commit

---
+++ js/tests/integration/bundle.js
@@ -0,0 +1,9 @@
+import 'popper.js'
+import {
+  Tooltip
+} from '../../../dist/js/bootstrap.esm.js'
+
+window.addEventListener('load', () => {
+  [...document.querySelectorAll('[data-toggle="tooltip"]')]
+    .map(tooltipNode => new Tooltip(tooltipNode))
+})
